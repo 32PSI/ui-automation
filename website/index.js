f2e912bdeb46ae31d949fc30e5689c43ea05feb8
Māris <51344125+32PSI@users.noreply.github.com>
2022-06-20 03:20:00 +0300

    added mock website

---
+++ website/index.js
@@ -0,0 +1,19 @@
+require('dotenv').config()
+const express = require('express')
+const {portValidator} = require('port-validator');
+const app = express()
+
+let port = 3000
+const envPort= parseInt(process.env.NODE_PORT)
+
+if (process.env.NODE_PORT && portValidator(envPort).validate()===true){
+  port = envPort
+}
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
