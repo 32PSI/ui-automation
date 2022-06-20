ce9e36285cabd473b10cc04eabc7a041537e7701
Māris <51344125+32PSI@users.noreply.github.com>
2022-06-20 05:00:41 +0300

    updates to the pipline shell scripts

--- website/build.sh
+++ website/build.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
 npm install
+if [ $? -eq 0 ]
+then
+    echo "Success of stage BUILD, Enviroment $1"
+    sh ../send_notification.sh "$1 build" 0
+    exit 0
+else
+    echo "Failure of stage BUILD, Enviroment $1"
+    sh ../send_notification.sh "$1 build" 1
+    exit 1
+fi
--- website/deploy.sh
+++ website/deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo "Starting deployment on $1 Enviroment"
 screen -XS $1 quit $ > /dev/null 2>&1
 if [ $1 == 'stage' ]
 then
@@ -7,3 +8,13 @@ else
     export NODE_PORT=3000
 fi
 screen -S $1 -dm node ./index.js
+if [ $? -eq 0 ]
+then
+    echo "Success of stage Deploy, Enviroment $1"
+    sh ../send_notification.sh "$1 deploy" 0
+    exit 0
+else
+    echo "Failure of stage Deploy, Enviroment $1"
+    sh ../send_notification.sh "$1 deploy" 1
+    exit 1
+fi
